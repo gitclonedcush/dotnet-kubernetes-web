e870155a672a2e7525aadae086bee8fef4546bc9
Daniel Cushing <35702025+cushind@users.noreply.github.com>
2019-01-09 10:23:15 -0700

    cushind/init-template (#1)
    Initial template setup with Dockerfile, Kubernetes deployment files, and sample asp.net core app.

---
+++ src/KubernetesApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using App.Metrics.AspNetCore.Health;
+using App.Metrics.Formatters.Prometheus;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace KubernetesApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .UseMetricsEndpoints(options => 
+                    options.MetricsEndpointOutputFormatter = new MetricsPrometheusTextOutputFormatter())
+                .UseMetricsWebTracking()
+                .UseHealth();
+    }
+}
